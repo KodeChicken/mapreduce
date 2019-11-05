db8dfd3f63245c50c7b6797bfeb952f54fb438e8
zhuyz <540864898@qq.com>
2019-11-05 22:07:52 +0800

    Initial commit

---
+++ src/main/java/com/zhuyz/flow/FlowDriver.java
@@ -0,0 +1,49 @@
+package com.zhuyz.flow;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowDriver {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        //1.获取Job实例
+        Job job = Job.getInstance(new Configuration());
+
+        //2.设置类路径
+        job.setJarByClass(FlowDriver.class);
+
+        //3.设置mapper和reducer
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+        //4.设置输入输入类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FlowBean.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        //5.设置输入输出路径
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+
+        System.exit(b ? 0 : 1);
+
+
+
+
+
+
+    }
+
+
+}
